c9aa23a1cb5e0c3e02da51e7709a811fb26cd65c
ercan kaplan <ercan05kaplan@gmail.com>
2022-11-19 17:34:21 +0300

    Add project files.

---
+++ EcoTravel.FligthEngine/Controllers/AmadeusGDSController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EcoTravel.FligthEngine.Controllers
+{
+    public class AmadeusGDSController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
